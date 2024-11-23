6703c2ebd9824e02db69f626651ea118dc341f89
Jagger <jiangyujian@duck.com>
2024-11-23 17:41:50 +0800

    delete number

--- src/routes/about.jsx
+++ src/routes/about.jsx
@@ -1,12 +1,15 @@
 import { Title } from "@solidjs/meta";
-import BigNumber from "bignumber.js";
+// import BigNumber from "bignumber.js";
 export default function Home() {
-  const number = new BigNumber(0.2).plus(0.1);
+  // const number = () => {
+  //   "use server";
+  //   return new BigNumber(0.2).plus(0.1).toString();
+  // };
   return (
     <main>
       <Title>About</Title>
       <h1>About</h1>
-      <p>{number.toString()}</p>
+      {/* <p>{number}</p> */}
     </main>
   );
 }
