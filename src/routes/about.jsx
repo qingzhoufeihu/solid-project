f18d78f18e5fac52a2b0692dcf5cb603505f0e40
Jagger <jiangyujian@duck.com>
2024-11-23 19:54:35 +0800

    test

--- src/routes/about.jsx
+++ src/routes/about.jsx
@@ -1,15 +1,15 @@
 import { Title } from "@solidjs/meta";
 import { createAsync } from "@solidjs/router";
-import { count } from "../components/test";
+// import { count } from "../components/test";
 export default function Home() {
-  const number = createAsync(() => count());
+  // const number = createAsync(() => count());
   // const number = createAsync(() => new BigNumber(0.2).plus(0.1).toString());
   // const number = new BigNumber(0.2).plus(0.1).toString();
   return (
     <main>
       <Title>About</Title>
       <h1>About</h1>
-      <p>{number}</p>
+      {/* <p>{number}</p> */}
     </main>
   );
 }
