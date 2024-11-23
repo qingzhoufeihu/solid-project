091279fb856a8209fb7d928c3baf4b9c0694ad98
Jagger <jiangyujian@duck.com>
2024-11-23 18:09:57 +0800

    test number

--- src/routes/about.jsx
+++ src/routes/about.jsx
@@ -1,10 +1,8 @@
 import { Title } from "@solidjs/meta";
 import BigNumber from "bignumber.js";
 export default function Home() {
-  const number = () => {
-    "use server";
-    return new BigNumber(0.2).plus(0.1).toString();
-  };
+  const number = new BigNumber(0.2).plus(0.1).toString();
+
   return (
     <main>
       <Title>About</Title>
