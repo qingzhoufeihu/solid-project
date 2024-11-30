907aee8b9efde9fe159f09d717d571ee4bb1356d
Jagger <jiangyujian@duck.com>
2024-11-30 22:28:07 +0800

    Update about.jsx

--- src/routes/about.jsx
+++ src/routes/about.jsx
@@ -1,16 +1,25 @@
 import { Title } from "@solidjs/meta";
 import { createAsync } from "@solidjs/router";
 import BigNumber from "bignumber.js";
+
+const getNotePreview = () => {
+  "use server";
+  return new BigNumber(0.2).plus(0.1).toString();
+};
+
+export const route = {
+  preload({ params }) {
+    getNotePreview();
+  },
+};
+
 export default function Home() {
-  const number = createAsync(() => {
-    // "use server";
-    return new BigNumber(0.2).plus(0.1).toString();
-  });
+  const number = createAsync(() => getNotePreview());
   // const number = new BigNumber(0.2).plus(0.1).toString();
   return (
     <main>
       <Title>About</Title>
-      <h1>About</h1>
+      <h1>About1</h1>
       <p>{number}</p>
     </main>
   );
